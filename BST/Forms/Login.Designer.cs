9033a185e308255a5548d9aca81645011592aab1
anastasiabholee <bholeeanastasia6@gmail.com>
2026-03-05 22:27:04 +0000

    Login Page Design + code

--- BST/Forms/Login.Designer.cs
+++ BST/Forms/Login.Designer.cs
@@ -28,12 +28,109 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            label1 = new Label();
+            label2 = new Label();
+            txtEmail = new TextBox();
+            txtPassword = new TextBox();
+            label3 = new Label();
+            btnLogin = new Button();
+            btnRegister = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 16F);
+            label1.Location = new Point(300, 29);
+            label1.Name = "label1";
+            label1.Size = new Size(194, 30);
+            label1.TabIndex = 0;
+            label1.Text = "CATAN BUS LOGIN";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(269, 117);
+            label2.Name = "label2";
+            label2.Size = new Size(36, 15);
+            label2.TabIndex = 1;
+            label2.Text = "Email";
+            // 
+            // txtEmail
+            // 
+            txtEmail.AccessibleName = "txtEmail";
+            txtEmail.Location = new Point(349, 114);
+            txtEmail.MaximumSize = new Size(200, 0);
+            txtEmail.Name = "txtEmail";
+            txtEmail.Size = new Size(200, 23);
+            txtEmail.TabIndex = 2;
+            // 
+            // txtPassword
+            // 
+            txtPassword.AccessibleName = "txtPassword";
+            txtPassword.Location = new Point(349, 159);
+            txtPassword.MaximumSize = new Size(200, 0);
+            txtPassword.Name = "txtPassword";
+            txtPassword.Size = new Size(200, 23);
+            txtPassword.TabIndex = 3;
+            txtPassword.UseSystemPasswordChar = true;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(269, 167);
+            label3.Name = "label3";
+            label3.Size = new Size(57, 15);
+            label3.TabIndex = 4;
+            label3.Text = "Password";
+            // 
+            // btnLogin
+            // 
+            btnLogin.AccessibleName = "btnLogin";
+            btnLogin.Location = new Point(300, 247);
+            btnLogin.Name = "btnLogin";
+            btnLogin.Size = new Size(100, 23);
+            btnLogin.TabIndex = 5;
+            btnLogin.Text = "Login";
+            btnLogin.UseVisualStyleBackColor = true;
+            btnLogin.Click += button1_Click;
+            // 
+            // btnRegister
+            // 
+            btnRegister.AccessibleName = "btnRegister";
+            btnRegister.Location = new Point(428, 247);
+            btnRegister.Name = "btnRegister";
+            btnRegister.Size = new Size(100, 23);
+            btnRegister.TabIndex = 6;
+            btnRegister.Text = "Register";
+            btnRegister.UseVisualStyleBackColor = true;
+            // 
+            // Login
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(btnRegister);
+            Controls.Add(btnLogin);
+            Controls.Add(label3);
+            Controls.Add(txtPassword);
+            Controls.Add(txtEmail);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "Login";
             Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
+        private Label label2;
+        private TextBox txtEmail;
+        private TextBox txtPassword;
+        private Label label3;
+        private Button btnLogin;
+        private Button btnRegister;
     }
 }
--- BST/Forms/Login.cs
+++ BST/Forms/Login.cs
@@ -1,3 +1,6 @@
+using BST.Services;
+using BST.Models;
+using BST.Forms;
 namespace BST
 {
     public partial class Login : Form
@@ -5,6 +8,38 @@ namespace BST
         public Login()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string email = txtEmail.Text;
+            string password = txtPassword.Text;
+
+            AuthenticationService authService = new AuthenticationService();
+
+            //User user = authService.Login(email, password);
+
+            //if (user != null)
+            //{
+            //    MessageBox.Show("Login Successful!");
+
+            //    if (user.IsAdmin)
+            //    {
+            //        AdminDashboardForm admin = new AdminDashboardForm();
+            //        admin.Show();
+            //    }
+            //    else
+            //    {
+            //        UserDashboardForm userDashboard = new UserDashboardForm();
+            //        userDashboard.Show();
+            //    }
+
+            //    this.Hide();
+            //}
+            //else
+            //{
+            //    MessageBox.Show("Invalid Email or Password");
+            //}
         }
     }
 }
