11545e4162aa4e082a04c50f097a73eb2a35b0ad
anastasiabholee <bholeeanastasia6@gmail.com>
2026-03-05 22:05:08 +0000

    Add project files.

---
+++ BST/Data/DatabaseService.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BST.Data
+{
+    public class DatabaseService
+    {
+        private string connectionString =
+            "Server=DESKTOP-47GUR0C\\SQLEXPRESS;Database=CatanBusDB;Trusted_Connection=True;";
+
+        public SqlConnection GetConnection()
+        {
+            return new SqlConnection(connectionString);
+        }
+    }
+}
---
+++ BST/Forms/Login.cs
@@ -0,0 +1,10 @@
+namespace BST
+{
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+    }
+}
